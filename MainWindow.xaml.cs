795c8333e3df67b348ca2dd401ad1481798886a6
bis5 <bis5@bis5.mydns.jp>
2014-02-14 10:26:13 +0900

    apply MahApps.Metro UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
-namespace JisResumeGenerater
-{
+namespace JisResumeGenerater {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() {
             InitializeComponent();
         }
+
     }
 }
